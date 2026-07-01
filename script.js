17ba335a69fcb44b776ddcf982dbec78732f50f9
Dannaha <72921845@continental.edu.pe>
2026-07-01 11:14:34 -0500

    productosscript

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+//1. Productos
 const productos = [
     {
         id: 1,
-        nombre:"Paste Chocolate",
+        nombre: "Pastel Chocolate",
         precio: 45,
         categoria: "Chocolate",
-        imagen:"https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=500",
-        descripcion: "Pastel de chocolate cobertura especial y ganache. perfecto para amantes del chocolate."
+        imagen: "https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=500",
+        descripcion: "Pastel de chocolate con cobertura especial y ganache. Perfecto para amantes del chocolate."
     },
     {
         id: 2,
-        nombre:"Paste Fresa",
+        nombre: "Pastel de Cumpleaños",
         precio: 40,
-        categoria: "Fruta",
-        imagen:"https://images.unsplash.com/photo-1464349095431-e9a21285b5f3?w=500",
-        descripcion: "Pastel suave conn fresas frescas y crema pastelera. Decorado con fresas enteras"
+        categoria: "Especial",
+        imagen: "https://images.unsplash.com/photo-1464349095431-e9a21285b5f3?w=500",
+        descripcion: "Pastel tricolor de fiestapara especialmentte para niños."
     },
     {
         id: 3,
@@ -25,36 +26,34 @@ const productos = [
     },
     {
         id: 4,
-        nombre: "Pastel de Naranja",
+        nombre: "Cupcake de Naranja",
         precio: 38,
         categoria: "Fruta",
-        imagen:"https://images.unsplash.com/photo-1563729784474-d77dbb933a9e?w=500",
-        descripcion: "Pastel artesanal sabor naranja con glaseado citrico y ralladura de naranja."
-
+        imagen: "https://images.unsplash.com/photo-1563729784474-d77dbb933a9e?w=500",
+        descripcion: "Cupcake artesanales sabor naranja cubierto con gaseado de fresa."
     },
     {
         id: 5,
-        nombre: "Pastel de Bodas",
-        precio: 108,
+        nombre: "Torta de Bodas",
+        precio: 180,
         categoria: "Especial",
-        imagen: "https://images.unsplash.com/photo-1621303837174-89787a7d4729?w=500",
-        descripcion: "Torta elegante de tres pisos para ocaciones especiales. Decorracion personalizada."
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx8AbrzUC5-XmkfWPMUkrGIq4N9yU1DlfSAE_ba-Un_6ZOQi6toL5b7zRJ&s=10",
+        descripcion: "Torta elegante de tres pisos para ocasiones especiales. Decoración personalizada."
     },
-
     {
         id: 6,
         nombre: "Cupcakes Surtidos",
         precio: 30,
         categoria: "Especial",
         imagen: "https://images.unsplash.com/photo-1486427944299-d1955d23e34d?w=500",
-        descripcion: "Cajas de 6 cupcakes surtidos con diferentes decoraciones y sabores."
+        descripcion: "Caja de 6 cupcakes surtidos con diferentes decoraciones y sabores."
     },
     {
         id: 7,
         nombre: "Pastel de Vainilla",
         precio: 42,
         categoria: "Clásico",
-        imagen: "https://images.unsplash.com/photo-1588340371950-a2ca583c4356?w=500",
+        imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_Y6iSdvVgFNURKjvNvFAtqxM6XwAzKLyJnRnuSdWKDS1wfxyiS_ka5CQ&s=10",
         descripcion: "Pastel de vainilla con crema de mantequilla y decoración de flores comestibles."
     },
     {
@@ -62,8 +61,8 @@ const productos = [
         nombre: "Torta Red Velvet",
         precio: 55,
         categoria: "Chocolate",
-        imagen: "https://images.unsplash.com/photo-1588195538326-c5b1e9f80a1b?w=500",
-        descripacion: "Clasica torta Red Velvet con crema de queso. Color rojo intenso y sabor inconfundible."
+        imagen: "https://i0.wp.com/www.pasionthermomix.co/wp-content/uploads/2022/10/0001005216LosMejoresPostresDelMundoEp007RedVelvetCake3.jpg?resize=768%2C432&ssl=1",
+        descripcion: "Clásica torta Red Velvet con crema de queso. Color rojo intenso y sabor inconfundible."
     }
+]
 
-];
